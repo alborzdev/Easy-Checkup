c23dbc55ad7a9b65e07f8ef402b9723ae7aabdfe
Alborz Gharabaghi <alborzgithub@gmail.com>
2021-02-06 16:43:40 -0500

    Added a bunch of UI stuff

---
+++ EasyCheckup1.0/src/application/checkupController.java
@@ -0,0 +1,30 @@
+package application;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+public class checkupController {
+	
+
+    @FXML
+    private Button btnCheckupTwo;
+
+    @FXML
+    private Button btnCheckupThree;
+
+    @FXML
+    private Button btnCheckupOne;
+    
+    @FXML
+    void initialize() {
+    	btnCheckupOne.setOnAction(event -> {
+    		System.out.println("Clicked");
+    	});
+    	btnCheckupTwo.setOnAction(event -> {
+    		System.out.println("Clicked");
+    	});
+    	btnCheckupThree.setOnAction(event -> {
+    		System.out.println("Clicked");
+    	});
+    }
+
+
+}
