16563ff2d37f22fb1463da935c48e0be93ad0799
Alborz Gharabaghi <alborzgithub@gmail.com>
2021-02-06 19:53:18 -0500

    not working

--- EasyCheckup1.0/src/application/AddQuestionContoller.java
+++ EasyCheckup1.0/src/application/AddQuestionContoller.java
@@ -1,5 +1,14 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class AddQuestionContoller {
-	
+
+    @FXML
+    private Button btnCheckupOne;
+
+    @FXML
+    private Button btnBack;
+
 }
